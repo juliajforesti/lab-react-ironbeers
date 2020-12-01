f827b970f135a99a06a551281ae31142042524b0
Julia Foresti <juliajforesti@gmail.com>
2020-12-01 14:16:38 -0300

    home images changed

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = (props) => {
   return (  
     <div className="card " style={{width: '100%'}}>
-      <img className="card-img-top" src="./images/beer-pub.jpg" alt="Card cap" />
+      <img className="card-img-top" src={`./images/${props.img}.jpg`} alt="Card cap" />
       <div className="card-body">
         <h5 className="card-title">{props.title}</h5>
         <p className="card-text">{props.text}</p>
--- src/routeComponents/Home.js
+++ src/routeComponents/Home.js
@@ -10,18 +10,21 @@ const Home = () => {
           <Card
             title="All Beers"
             text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam iure repellat quia molestias reiciendis animi saepe dolorum tempore voluptatem obcaecati?"
+            img='beer-pub'
           />
         </Link>
         <Link className="col-12 col-lg-4" to="random-beer">
           <Card
             title="Random Beer"
             text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam iure repellat quia molestias reiciendis animi saepe dolorum tempore voluptatem obcaecati?"
+            img='random-beer'
           />
         </Link>
         <Link className="col-12 col-lg-4" to="new-beer">
           <Card
             title="New Beer"
             text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam iure repellat quia molestias reiciendis animi saepe dolorum tempore voluptatem obcaecati?"
+            img='new-beer'
           />
         </Link>
       </div>
